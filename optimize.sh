4b22cdc583e36840a7dba1814485e9c3e0cdc944
JoshuaHall <joshuahallmail@gmail.com>
2020-05-19 12:42:39 -0500

    Made optimize.sh format the src/ directory

--- optimize.sh
+++ optimize.sh
@@ -5,6 +5,8 @@ set -e
 js="dist/elm.js"
 min="dist/elm.min.js"
 
+elm-format src/ --yes
+
 elm make --optimize --output=$js "$@"
 
 uglifyjs $js --compress 'pure_funcs="F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9",pure_getters,keep_fargs=false,unsafe_comps,unsafe' | uglifyjs --mangle --output=$min
